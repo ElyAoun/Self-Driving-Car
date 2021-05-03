0cc11da61e7f5326bf78b9f50f1a36fc55321b72
jason <jasonabisaad@hotmail.com>
2021-05-03 18:41:06 +0300

    Car fixed

--- WebcamModule.py
+++ WebcamModule.py
@@ -1,23 +1,7 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-
-
-def getImg(display=False, size=(480, 240)):
-    _, img = cap.read()
-    img = cv2.resize(img, (size[0], size[1]))
-    if display:
-        cv2.imshow('IMG', img)
-    return img
-
 class Webcam():
     def __init__(self,captureNumber,screenWidth=320,screenHeight=240):
         self.capture=cv2.VideoCapture(captureNumber)
         self.capture.set(3,screenWidth)
         self.capture.set(4,screenHeight)
-
-
-
-if __name__ == '__main__':
-    while True:
-        img = getImg(True)
