aad577075865872f1a6dfd1f69096dcc5fbf42b5
jason <jasonabisaad@hotmail.com>
2021-05-06 17:02:34 +0300

    Car fixed

--- WebcamModule.py
+++ WebcamModule.py
@@ -1,8 +1,11 @@
 import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+
+
 class Webcam():
-    def __init__(self,screenWidth=320,screenHeight=240):
-        self.camera=PiCamera()
+    def __init__(self, screenWidth=320, screenHeight=240):
+        self.camera = PiCamera()
         self.camera.resolution = (screenWidth, screenHeight)
-        self.rawCapture = PiRGBArray(self.camera, size=(screenWidth, screenHeight))
+        self.camera.framerate = 30
+        self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
